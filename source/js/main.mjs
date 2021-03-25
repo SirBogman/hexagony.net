31fcecd915cf59dd8a9ad516472493b8a9cd15d8
Brian Collins <bri.l.collins@gmail.com>
2021-03-25 16:21:01 -0400

    Don't show edge transition animations when running at high speed.

--- source/js/main.mjs
+++ source/js/main.mjs
@@ -330,7 +330,8 @@ function startEdgeAnimation(connectors, name) {
 }
 
 function edgeEventHandler(edgeName, isBranch) {
-    if (userData.edgeTransitionMode) {
+    // Don't show edge transition animations when running at high speed.
+    if (userData.edgeTransitionMode && userData.delay) {
         const name = isBranch ? 'connector_flash' : 'connector_neutral_flash';
         startEdgeAnimation(gridView.edgeConnectors[edgeName], name);
         startEdgeAnimation(gridView.edgeConnectors2[edgeName], `${name}_secondary`);
