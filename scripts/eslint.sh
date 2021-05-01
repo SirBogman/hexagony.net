31ce33ac9bb61b8f9882dfa590e35fcfebe993e1
Brian Collins <bri.l.collins@gmail.com>
2021-04-30 23:18:40 -0400

    Update eslint script to avoid the need to install eslint globally.

--- scripts/eslint.sh
+++ scripts/eslint.sh
@@ -1,2 +1,2 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-eslint --ext .mjs,.jsx,.js --ignore-path "${DIR}/../.gitignore" "${DIR}/../" "$@"
+npx eslint --ext .mjs,.jsx,.js --ignore-path "${DIR}/../.gitignore" "${DIR}/../" "$@"
